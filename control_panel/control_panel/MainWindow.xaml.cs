1aa53219eede0df9b1caec3f0d523299b557245d
ecto0310 <ecto0310@gmail.com>
2019-09-05 01:13:27 +0900

    Add sound load

--- control_panel/control_panel/MainWindow.xaml.cs
+++ control_panel/control_panel/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,42 @@ namespace control_panel
   /// </summary>
   public partial class MainWindow : Window
   {
+    private SoundPlayer soundQuestion = new SoundPlayer();
+    private SoundPlayer soundCorrect = new SoundPlayer();
+    private SoundPlayer soundWrong = new SoundPlayer();
+    private SoundPlayer soundBuzzer = new SoundPlayer();
+
     public MainWindow()
     {
       InitializeComponent();
+      Initialize();
+    }
+
+    /// <summary>
+    /// Initialization process
+    /// </summary>
+    private void Initialize()
+    {
+      if (ConfigurationManager.AppSettings["PathSoundQuestion"] != null)
+      {
+        soundQuestion.SoundLocation = ConfigurationManager.AppSettings["PathSoundQuestion"];
+        soundQuestion.Load();
+      }
+      if (ConfigurationManager.AppSettings["PathSoundCorrect"] != null)
+      {
+        soundCorrect.SoundLocation = ConfigurationManager.AppSettings["PathSoundCorrect"];
+        soundCorrect.Load();
+      }
+      if (ConfigurationManager.AppSettings["PathSoundWrong"] != null)
+      {
+        soundWrong.SoundLocation = ConfigurationManager.AppSettings["PathSoundWrong"];
+        soundWrong.Load();
+      }
+      if (ConfigurationManager.AppSettings["PathSoundBuzzer"] != null)
+      {
+        soundBuzzer.SoundLocation = ConfigurationManager.AppSettings["PathSoundBuzzer"];
+        soundBuzzer.Load();
+      }
     }
   }
 }
