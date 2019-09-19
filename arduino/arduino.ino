a2089ed5ea91fd2d361c5eeae3dce6fd6db2a8ce
ecto0310 <ecto0310@gmail.com>
2019-09-19 23:45:49 +0900

    Add initialization process

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,27 @@
+const int cntPlayer = 5;
+
+//Structure of player state
+struct PlayerStatus
+{
+  const int Input, Output;
+  int Status, Finish;
+};
+
+PlayerStatus playerStatus[5] = {{2, 3, -1, 0}, {4, 5, -1, 0}, {6, 7, -1, 0}, {8, 9, -1, 0}, {10, 11, -1, 0}};
+int solver[5] = {-1, -1, -1, -1, -1};
+int now = 0, receive = 0;
+
 void setup()
 {
+  //Set serial
+  Serial.begin(9600);
+
+  //Set player
+  for (int i = 0; i < cntPlayer; i++)
+  {
+    pinMode(playerStatus[i].Input, INPUT);
+    pinMode(playerStatus[i].Output, OUTPUT);
+  }
 }
 
 void loop()
